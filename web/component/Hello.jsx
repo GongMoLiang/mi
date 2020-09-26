af48ba13239c075fb1aee70a195f71c3e1b9c091
GongMoLiang <gml18720826651@163.comm>
2020-09-26 14:31:23 +0800

    检查

--- web/component/Hello.jsx
+++ web/component/Hello.jsx
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Button } from 'antd'
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { Button, Row } from "antd"
 
-@connect( store => store)
+@connect(store => store)
 export default class Hello extends Component {
-    handleAge = () =>{
-        const { dispatch } = this.props 
-        dispatch({
-            type: 'CHANGE_AGE',
-            age: 20
-        })
-    }
-    render(){
-        const { name, age } = this.props
-        return (
-            <div>
-                <div>{ name }</div>
-                <div>{ age }</div>
-                <Button onClick = { this.handleAge } type="primary">点击修改名字</Button>
-            </div>
-        )
-    }
+	handleAge = () => {
+		const { dispatch } = this.props
+		dispatch({
+			type: "CHANGE_AGE",
+			age: 20
+		})
+	}
+	render() {
+		const { name, age } = this.props
+		return (
+			<div>
+				<div>{name}</div>
+				<div>{age}</div>
+				<Button onClick={this.handleAge} type="primary">
+					点击修改名字
+				</Button>
+			</div>
+		)
+	}
 }
